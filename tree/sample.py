cb0d5e30a879dacc5aa126a93d248e60546721fe
hprasad <hrishikeshprasadc@gmail.com>
2023-12-20 09:58:05 +0530

    sample workouts

--- tree/sample.py
+++ tree/sample.py
@@ -1,180 +1,19 @@
-class BST:
-    def __init__(self,key):
-        self.key=key
-        self.lchild=None
-        self.rchild=None
-
-    def insert(self,data):
-        if self.key is None:
-            self.key=data
-            return
-        if self.key==data:
-            return
-        if self.key>data:
-            if self.lchild:
-                self.lchild.insert(data)
-            else:
-                self.lchild=BST(data)
-        else:
-            if self.rchild:
-                self.rchild.insert(data)
-            else:
-                self.rchild=BST(data)
-    
-    def preorder(self):
-        print(self.key,end=' ')
-        if self.lchild:
-            self.lchild.preorder()
-        if self.rchild:
-            self.rchild.preorder()
-
-    def inorder(self):
-        if self.lchild:
-            self.lchild.inorder()
-        print(self.key,end=' ')
-        if self.rchild:
-            self.rchild.inorder()
-    def postorder(self):
-        if self.lchild:
-            self.lchild.postorder()
-        if self.rchild:
-            self.rchild.postorder()
-        print(self.key,end=' ')
-
-    def search(self,data):
-        if self.key==data:
-            print('found!')
-            return
-        if self.key>data:
-            if self.lchild:
-                self.lchild.search(data)
-            else:
-                print('not found!')
-        else:
-            if self.rchild:
-                self.rchild.search(data)
-            else:
-                print('not found!')
-    
-    def delete(self,data,curr):
-        if not self.key:
-            print('Empty tree!')
-            return
-        if self.key>data:
-            if self.lchild:
-                self.lchild=self.lchild.delete(data,curr)
-            else:
-                print('not found in tree!')
-        elif self.key<data:
-            if self.rchild:
-                self.rchild=self.rchild.delete(data,curr)
-            else:
-                print('Not found in tree!')
-        else:
-            if not self.lchild:
-                temp=self.rchild
-                if curr==data:
-                    self.key=temp.key
-                    self.rchild=temp.rchild
-                    self.lchild=temp.lchild
-                    temp= None
-                    return
-                self=None
-                return temp
-            if not self.rchild:
-                temp=self.lchild
-                if curr==data:
-                    self.key=temp.key
-                    self.lchild=temp.lchild
-                    self.rchild=temp.rchild
-                    temp=None
-                    return 
-                self=None
-                return temp
-            node=self.rchild
-            while node.lchild:
-                node=node.lchild
-            self.key=node.key
-            self.rchild=self.rchild.delete(node.key,curr)
-        return self
-    
-    def min_node(self):
-        node=self
-        while node.lchild:
-            node=node.lchild
-        print(node.key)
-        return    
-    def max_node(self):
-        node=self
-        while node.rchild:
-            node=node.rchild
-        print(node.key)
-        return
-            
-
-
-
-def count(root):
-    if not root:
-        return 0
-    else:
-        return 1+count(root.lchild)+count(root.rchild)
-
-
-root=BST(None)
-arr=[2,4,5,6,77,8,3,45,7]
-
-for i in arr:
-    root.insert(i)
-    
-root.preorder()
-print('preorder')
-root.inorder()
-print('inorder')
-root.postorder()
-print('postorder')
-
-root.search(7)
-
-if count(root)>1:
-    root.delete(2,root.key)
-else:
-    print('cannot be deleted!')
-
-root.postorder()
-print('postorder')
-
-print('Min Node of the tree!')
-root.min_node()
-print('max Node of the tree!')
-root.max_node()
-
-
-#HEAP
-print('--------Heap DS---------')
 import heapq
-list1=[]
-heapq.heappush(list1,12)
-heapq.heappush(list1,1)
-heapq.heappush(list1,16)
-heapq.heappush(list1,21)
-heapq.heappush(list1,17)
-heapq.heappush(list1,5)
-heapq.heappush(list1,2)
-print(list1)
 
-heapq.heappop(list1)
-heapq.heappushpop(list1,3)
-heapq.heapreplace(list1,6)
-print(list1)
-list2=[2,6,6,7,34,23,12,56,565]
-heapq.heapify(list2)
-print(list2)
-print(heapq.nsmallest(3,list2))
-print(heapq.nlargest(3,list2))
+arr=[12,34,654,76,34,23,4,5]
+heapq.heapify(arr)
 
-list3=[(1,'hrishi'),(1,'prasad'),(3,'hrishikesh')]
+heapq.heappush(arr,13)
+heapq.heappop(arr)
+heapq.heappushpop(arr,11)
+heapq.heapreplace(arr,1)
+print(heapq.nsmallest(5,arr))
+print(heapq.nlargest(5,arr))
+print(arr)
 
+
+#Priority Queue
+list3=[[31,'Hrishi'],[12,'Arathy'],[53,'Ammu'],[1,'mohanlal']]
 heapq.heapify(list3)
-for i in range(len(list3)):
-    print(heapq.heappop(list3))
+for i in list3:
+    print(i)
--- tree/sample2.py
+++ tree/sample2.py
@@ -1,29 +1,208 @@
-def heapify(arr,n,i):
-    larg=i
-    lchild=2*i+1
-    rchild=2*i+2
-
-    if lchild<n and arr[lchild]>arr[larg]:
-        larg=lchild
-
-    if rchild<n and arr[rchild]>arr[larg]:
-        larg=rchild
-
-    if larg!=i:
-        arr[i],arr[larg]=arr[larg],arr[i]
-        heapify(arr,n,larg)
-
-def heap_sort(arr):
-    n=len(arr)
-
-    for i in range(n//2-1,-1,-1):
-        heapify(arr,n,i)
+class BST:
+    def __init__(self,key):
+        self.key=key
+        self.lchild=None
+        self.rchild=None
     
-    for i in range(n-1,-1,-1):
-        arr[0],arr[i]=arr[i],arr[0]
-    
-        heapify(arr,i,0)
+    def insert(self,data):
+        if not self.key:
+            self.key=data
+            return
+        if self.key==data:
+            return
+        if self.key>data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                self.lchild=BST(data)
+        else:
+            if self.rchild:
+                self.rchild.insert(data)
+            else:
+                self.rchild=BST(data)
 
-arr = [12, 11, 13, 5, 6, 7]
-heap_sort(arr)
-print("Sorted array:", arr)
+    def validate(self):
+        if not self.key:
+            print('empty root or tree!')
+            return False
+        if self.lchild and self.lchild.key>=self.key:
+            return False
+        if self.rchild and self.rchild.key<=self.key:
+            return False
+        
+        if self.lchild and not self.lchild.validate():
+            return False
+        if self.rchild and not self.rchild.validate():
+            return False
+        return True
+    
+    def preorder(self):
+        print(self.key,end=' ')
+        if self.lchild:
+            self.lchild.preorder()
+        if self.rchild:
+            self.rchild.preorder()
+    
+    def inorder(self):
+        if self.lchild:
+            self.lchild.inorder()
+        print(self.key,end=' ')
+        if self.rchild:
+            self.rchild.inorder()
+
+    def postorder(self):
+        if self.lchild:
+            self.lchild.postorder()
+        if self.rchild:
+            self.rchild.postorder()
+        print(self.key,end=' ')
+
+    def delete_node(self,data,curr):
+        if self.key>data:
+            if self.lchild:
+                self.lchild=self.lchild.delete_node(data,curr)
+            else:
+                print('Node not Found!')
+        elif self.key<data:
+            if self.rchild:
+                self.rchild=self.rchild.delete_node(data,curr)
+            else:
+                print('Node not Found!')
+        else:
+            if not self.lchild:
+                temp=self.rchild
+                if data==curr:
+                    self.key=temp.key
+                    self.lchild=temp.lchild
+                    self.rchild=temp.rchild
+                    return
+                self=None
+                return temp
+            if not self.rchild:
+                temp=self.lchild
+                if data==curr:
+                    self.key=temp.key
+                    self.lchild=temp.lchild
+                    self.rchild=temp.rchild
+                    return
+
+                self=None
+                return temp
+            node=self.rchild
+            while node.lchild:
+                node=node.lchild
+
+            self.key=node.key
+            self.rchild=self.rchild.delete_node(node.key,curr)
+        return self
+    
+    def Min_node(self):
+        node=self
+        while node.lchild:
+            node=node.lchild
+        print('Min node of the Tree is ',node.key)
+        return
+    
+    def Max_node(self):
+        node=self
+        while node.rchild:
+            node=node.rchild
+        print('Max node of the tree is :',node.key)
+        return 
+
+def count(node):
+    if not node:
+        return 0
+    else:
+        return 1+count(node.lchild)+count(node.rchild)
+
+
+root=BST(None)
+arr=[1,15,4,7,2,3,56,67,8,54,2,4,5,78990]
+for i in arr:
+    root.insert(i)
+
+if root.validate():
+    print('Tree is a BST!')
+else:
+    print('Not a BST!')
+
+print('----------Preorder----------')
+root.preorder()
+print()
+print('----------Postorder----------')
+root.postorder()
+print()
+print('-------------------Inorder------------')
+root.inorder()
+
+
+
+print()
+if count(root)>1:
+    print('---------After deletion----------')
+    root.delete_node(1,root.key)
+else:
+    print('Node cannot be deleted!')
+root.preorder()
+
+
+
+
+print()
+print('-----------------------------------')
+root.Min_node()
+print('-----------------------------------')
+root.Max_node()
+
+
+
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.is_complete=False
+
+class Trie:
+    def __init__(self):
+        self.root=TrieNode()
+    
+    def insert(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char]=TrieNode()
+            node=node.children[char]
+        node.is_complete=True 
+
+    def search(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node=node.children[char]
+        return node.is_complete
+    
+    def prefix(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node=node.children[char]
+        return True
+    
+print()
+print('-----TRIE-----')
+root=Trie()
+arr=['London','NewYork','Berlin','Ottawa','New Delhi']
+for i in arr:
+    root.insert(i)
+
+print('New Delhi is in Trie: ',root.search('New Delhi'))
+print('London is in Trie: ',root.search('London'))
+print('Paris is in Trie: ',root.search('Paris'))
+print('Mumbai is in Trie: ',root.search('Mumbai'))
+print('Word starting with Lon is there in Trie: ',root.prefix('Lon'))
+print('Word starting with New is there in Trie: ',root.prefix('New'))
+print('Word starting with Mum is there in Trie: ',root.prefix('Mum'))
+print('Word starting with Otta is there in Trie: ',root.prefix('Otta'))
+
